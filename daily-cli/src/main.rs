9a53cbaf002e1338be1688bbafd926fbc4cfd801
Logan Bjork <loganbjork@ufl.edu>
2025-12-11 00:45:45 -0500

    chop cheese config file lamo

--- daily-cli/src/main.rs
+++ daily-cli/src/main.rs
@@ -1,8 +1,20 @@
+use std::collections::HashMap;
 use std::fs;
 use std::io;
 use std::io::prelude::*;
 use chrono::{Local};
 
+// TODO refactor into lib crate cause ts butters
+
+// TODO all structs
+struct Config {
+    tasks: Vec<String>,
+}
+
+struct DailyFile {
+    file_path: String,
+    checklist: HashMap<String, bool>,
+}
 
 
 fn start_up() -> String {
@@ -18,56 +30,130 @@ fn start_up() -> String {
     today.to_string()
 }
 
+fn load_config() -> Config {
+    let mut created = false;
+
+    let mut config_open = match fs::File::open(&"config.log") {
+        Ok(file) => {
+            println!("Config file successfully located...");
+            file
+        }
+
+        Err(error) if error.kind() == io::ErrorKind::NotFound => {
+            println!("Config file undetected, creating default...");
+            
+            created = true;
+            fs::File::create(&"config.log").unwrap_or_else(|e| { panic!("Problem creating file: {e:?}") } )
+        }
+
+        Err(error) => {
+            panic!("Problem opening config: {error:?}");
+        }
+    };
+
+    if created {
+        config_open.write_all(b"Task1,Task2,Task3");
+    }
+
+    // READ FILE NAOW
+    let mut cft = String::new();
+    config_open.read_to_string(&mut cft).unwrap();
+    let t: Vec<String> = cft.split(',').map(|s| s.to_string()).collect();
+
+    let c = Config {
+        tasks: t.clone(),
+    };
+
+    println!("{:?}", c.tasks);
+
+    c
+}
+
 fn try_open(fname: &String, td: String) -> (bool, String) {
     // TODO: make file struct that is just filled from here
     // stores stuff like pathname and prolly hashmap of task names and vals
     let mut folder_path = String::from("days/");
+    let mut created = false;
 
     if fname.trim() == "today" {
         folder_path.push_str(&td);
         // println!("{folder_path}");
+        
+        // can probably refactor below somehow without having to copy paste in both ifs
+        let mut file_open = match fs::File::open(&folder_path) {
+            Ok(file) => {
+                println!("File '{}' exists, opening...", folder_path);
+                return (true, folder_path)
+            }
 
-        let mut file_open = fs::File::open(&folder_path).unwrap_or_else(|error| {
-            if error.kind() == io::ErrorKind::NotFound {
-                fs::File::create(&folder_path).unwrap_or_else(|error| {
-                    panic!("Problem creating file: {error:?}");
-                })
-            } else {
+            Err(error) if error.kind() == io::ErrorKind::NotFound => {
+                println!("File {}' not found, creating...", folder_path);
+                created = true;
+                fs::File::create(&folder_path).unwrap_or_else(|e| { panic!("Problem creating file: {e:?}") } )
+            }
+
+            Err(error) => {
                 panic!("Problem opening file: {error:?}");
             }
-        });
+        };
+        
+        if created {
+            println!("Writing boilerplate...");
+            let _ = file_open.write_all(b"task1,task2,task3,task4\nfalse,false,false,false");
+        }
 
-        let _ = file_open.write_all(b"task1,task2,task3,task4\nfalse,false,false,false");
-
-        return (true, folder_path)
+        // (true, folder_path)
 
     } else { // TODO: validate string to ensure its in YYYYMMDD format. ignore for now 
         folder_path.push_str(&fname.trim());
         // println!("{folder_path}");
 
-        let mut file_open = fs::File::open(&folder_path).unwrap_or_else(|error| { // might be able to
-                                                                               // just remove let
-                                                                               // but idk yet
-            if error.kind() == io::ErrorKind::NotFound {
-                fs::File::create(&folder_path).unwrap_or_else(|error| {
-                    panic!("Problem creating file: {error:?}");
-                })
-            } else {
+        let mut file_open = match fs::File::open(&folder_path) {
+            Ok(file) => {
+                println!("File '{}' exists, opening...", folder_path);
+                file
+            }
+
+            Err(error) if error.kind() == io::ErrorKind::NotFound => {
+                println!("File {}' not found, creating...", folder_path);
+                created = true;
+                fs::File::create(&folder_path).unwrap_or_else(|e| { panic!("Problem creating file: {e:?}") } )
+            }
+
+            Err(error) => {
                 panic!("Problem opening file: {error:?}");
             }
-        });
+        };
 
+        if created {
+            println!("Writing boilerplate..."); // FIXME boilerplate should be based on struct
+            let _ = file_open.write_all(b"task1,task2,task3,task4\nfalse,false,false,false");
+        }
 
-        let _ = file_open.write_all(b"task1,task2,task3,task4\nfalse,false,false,false");
-
-        return (true, folder_path)
+        // (true, folder_path)
     }
 
      // return (false, String::from(""))
+     // ^^ shouldnt be necessary bc error handling but anyway...s ^^
+    
+    let mut df = DailyFile {
+        file_path: folder_path.clone(),
+        checklist: HashMap::from(
+            [
+                (String::from("LeetCode"), false), 
+                (String::from("Practice"), false), 
+                (String::from("Program"), false), 
+                (String::from("Read"), false),
+            ]
+        ), // FIXME: will need to be refactored for customization (so not hardcoded tasks). probably through a config.txt. But that's for later. lamo
+    };
+    
+    return (true, folder_path)
 }
 
 fn main() {
     let today = start_up();
+    let conf = load_config();
     println!("What file would you like to open?");
 
     let mut file_name = String::new();
@@ -76,7 +162,12 @@ fn main() {
 
     if pair.0 {
         // println!("File exists");
-        let contents = fs::read_to_string(&pair.1).expect("Should read");
+        let contents = fs::read_to_string(&pair.1).expect("Should read"); // FIXME read from struct
+                                                                          // for better printing
+                                                                          // that wont make yo
+                                                                          // uwanna
+                                                                          // blelopyeroityoihsdfjkfm
+                                                                          // sdklfg,n jk
         println!("{}", contents);
     }
 
